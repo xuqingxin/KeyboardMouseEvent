5b18f00300c0da63a4b6a7108668eb105b623110
徐庆新 <xuqingxin@gmail.com>
2021-03-04 10:21:21 +0800

    First Commit

---
+++ KeyboardMouseEvent/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace KeyboardMouse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtAppTitle = new System.Windows.Forms.TextBox();
+            this.txtHwnd = new System.Windows.Forms.TextBox();
+            this.txtClassname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.btnSendMouse = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMouseX = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtMouseY = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cmbMouseKey = new System.Windows.Forms.ComboBox();
+            this.btnSendKey = new System.Windows.Forms.Button();
+            this.btnGroupSend = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtRepeatTimes = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(126, 190);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "查找";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtAppTitle
+            // 
+            this.txtAppTitle.Location = new System.Drawing.Point(100, 36);
+            this.txtAppTitle.Name = "txtAppTitle";
+            this.txtAppTitle.Size = new System.Drawing.Size(145, 21);
+            this.txtAppTitle.TabIndex = 2;
+            this.txtAppTitle.Text = "微信";
+            // 
+            // txtHwnd
+            // 
+            this.txtHwnd.Location = new System.Drawing.Point(100, 139);
+            this.txtHwnd.Name = "txtHwnd";
+            this.txtHwnd.Size = new System.Drawing.Size(145, 21);
+            this.txtHwnd.TabIndex = 3;
+            // 
+            // txtClassname
+            // 
+            this.txtClassname.Location = new System.Drawing.Point(100, 87);
+            this.txtClassname.Name = "txtClassname";
+            this.txtClassname.Size = new System.Drawing.Size(145, 21);
+            this.txtClassname.TabIndex = 4;
+            this.txtClassname.Text = "WeChatMainWndForPC";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "窗口名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "窗口类名";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "窗口句柄";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(337, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "按键";
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(372, 36);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(145, 21);
+            this.txtKey.TabIndex = 8;
+            this.txtKey.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtKey_KeyDown);
+            // 
+            // btnSendMouse
+            // 
+            this.btnSendMouse.Location = new System.Drawing.Point(548, 137);
+            this.btnSendMouse.Name = "btnSendMouse";
+            this.btnSendMouse.Size = new System.Drawing.Size(75, 23);
+            this.btnSendMouse.TabIndex = 10;
+            this.btnSendMouse.Text = "发送";
+            this.btnSendMouse.UseVisualStyleBackColor = true;
+            this.btnSendMouse.Click += new System.EventHandler(this.btnSendMouseKey_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(313, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "鼠标坐标";
+            // 
+            // txtMouseX
+            // 
+            this.txtMouseX.Location = new System.Drawing.Point(389, 87);
+            this.txtMouseX.Name = "txtMouseX";
+            this.txtMouseX.Size = new System.Drawing.Size(52, 21);
+            this.txtMouseX.TabIndex = 11;
+            this.txtMouseX.Text = "200";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(372, 90);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(11, 12);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "X";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(448, 90);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(11, 12);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Y";
+            // 
+            // txtMouseY
+            // 
+            this.txtMouseY.Location = new System.Drawing.Point(465, 87);
+            this.txtMouseY.Name = "txtMouseY";
+            this.txtMouseY.Size = new System.Drawing.Size(52, 21);
+            this.txtMouseY.TabIndex = 15;
+            this.txtMouseY.Text = "200";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(313, 142);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 12);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "鼠标按键";
+            // 
+            // cmbMouseKey
+            // 
+            this.cmbMouseKey.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMouseKey.FormattingEnabled = true;
+            this.cmbMouseKey.Items.AddRange(new object[] {
+            "左键",
+            "中键",
+            "右键"});
+            this.cmbMouseKey.Location = new System.Drawing.Point(372, 139);
+            this.cmbMouseKey.Name = "cmbMouseKey";
+            this.cmbMouseKey.Size = new System.Drawing.Size(145, 20);
+            this.cmbMouseKey.TabIndex = 18;
+            // 
+            // btnSendKey
+            // 
+            this.btnSendKey.Location = new System.Drawing.Point(548, 34);
+            this.btnSendKey.Name = "btnSendKey";
+            this.btnSendKey.Size = new System.Drawing.Size(75, 23);
+            this.btnSendKey.TabIndex = 19;
+            this.btnSendKey.Text = "发送";
+            this.btnSendKey.UseVisualStyleBackColor = true;
+            this.btnSendKey.Click += new System.EventHandler(this.btnSendKey_Click);
+            // 
+            // btnGroupSend
+            // 
+            this.btnGroupSend.Location = new System.Drawing.Point(442, 190);
+            this.btnGroupSend.Name = "btnGroupSend";
+            this.btnGroupSend.Size = new System.Drawing.Size(75, 23);
+            this.btnGroupSend.TabIndex = 20;
+            this.btnGroupSend.Text = "组合发送";
+            this.btnGroupSend.UseVisualStyleBackColor = true;
+            this.btnGroupSend.Click += new System.EventHandler(this.btnGroupSend_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(313, 195);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 12);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "重复次数";
+            // 
+            // txtRepeatTimes
+            // 
+            this.txtRepeatTimes.Location = new System.Drawing.Point(372, 192);
+            this.txtRepeatTimes.Name = "txtRepeatTimes";
+            this.txtRepeatTimes.Size = new System.Drawing.Size(52, 21);
+            this.txtRepeatTimes.TabIndex = 21;
+            this.txtRepeatTimes.Text = "10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 234);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtRepeatTimes);
+            this.Controls.Add(this.btnGroupSend);
+            this.Controls.Add(this.btnSendKey);
+            this.Controls.Add(this.cmbMouseKey);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtMouseY);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtMouseX);
+            this.Controls.Add(this.btnSendMouse);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtClassname);
+            this.Controls.Add(this.txtHwnd);
+            this.Controls.Add(this.txtAppTitle);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtAppTitle;
+        private System.Windows.Forms.TextBox txtHwnd;
+        private System.Windows.Forms.TextBox txtClassname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Button btnSendMouse;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMouseX;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtMouseY;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox cmbMouseKey;
+        private System.Windows.Forms.Button btnSendKey;
+        private System.Windows.Forms.Button btnGroupSend;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtRepeatTimes;
+    }
+}
+
---
+++ KeyboardMouseEvent/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyboardMouse
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")] private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll")] private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")] private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")] private static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")] private static extern bool SetCursorPos(int X, int Y);
+
+        [DllImport("user32.dll")] private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        [DllImport("user32.dll")] private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+
+        [DllImport("user32.dll")] private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndlnsertAfter, int X, int Y, int cx, int cy, uint Flags);
+
+        //ShowWindow参数
+        private const int SW_SHOWNORMAL = 1;
+
+        private const int SW_RESTORE = 9;
+        private const int SW_SHOWNOACTIVATE = 4;
+
+        //SendMessage参数
+        private const int WM_KEYDOWN = 0X100;
+
+        private const int WM_KEYUP = 0X101;
+        private const int WM_SYSCHAR = 0X106;
+        private const int WM_SYSKEYUP = 0X105;
+        private const int WM_SYSKEYDOWN = 0X104;
+        private const int WM_CHAR = 0X102;
+        private const int WM_LBUTTONDOWN = 0x0201;
+        private const int WM_LBUTTONUP = 0x0202;
+        private const int WM_MBUTTONDOWN = 0x0207;
+        private const int WM_MBUTTONUP = 0x0208;
+        private const int WM_RBUTTONDOWN = 0x0204;
+        private const int WM_RBUTTONUP = 0x0205;
+
+        [Flags]
+        public enum MouseEventFlags : uint
+        {
+            LEFTDOWN = 0x00000002,
+            LEFTUP = 0x00000004,
+            MIDDLEDOWN = 0x00000020,
+            MIDDLEUP = 0x00000040,
+            MOVE = 0x00000001,
+            ABSOLUTE = 0x00008000,
+            RIGHTDOWN = 0x00000008,
+            RIGHTUP = 0x00000010,
+            WHEEL = 0x00000800,
+            XDOWN = 0x00000080,
+            XUP = 0x00000100
+        }
+
+        //Use the values of this enum for the 'dwData' parameter
+        //to specify an X button when using MouseEventFlags.XDOWN or
+        //MouseEventFlags.XUP for the dwFlags parameter.
+        public enum MouseEventDataXButtons : uint
+        {
+            XBUTTON1 = 0x00000001,
+            XBUTTON2 = 0x00000002
+        }
+
+        public delegate bool CallBack(int hwnd, int lParam);
+
+        [DllImport("user32")] public static extern int EnumWindows(CallBack x, int y);
+
+        [DllImport("user32.dll")] private static extern int GetWindowTextW(IntPtr hWnd, [MarshalAs(UnmanagedType.LPWStr)] StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll")] private static extern int GetClassNameW(IntPtr hWnd, [MarshalAs(UnmanagedType.LPWStr)] StringBuilder lpString, int nMaxCount);
+
+        private IntPtr hwnd;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cmbMouseKey.SelectedIndex = 0;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            hwnd = FindWindow(txtClassname.Text, txtAppTitle.Text); //null为类名，可以用Spy++得到，也可以为空
+            txtHwnd.Text = hwnd.ToString("X");
+            ShowWindow(hwnd, SW_RESTORE);
+            SetForegroundWindow(hwnd);
+        }
+
+        public bool Recall(int hwnd, int lParam)
+        {
+            StringBuilder sb = new StringBuilder(256);
+            IntPtr PW = new IntPtr(hwnd);
+
+            GetWindowTextW(PW, sb, sb.Capacity); //得到窗口名并保存在strName中
+            string strName = sb.ToString();
+            Console.WriteLine(strName);
+
+            GetClassNameW(PW, sb, sb.Capacity); //得到窗口类名并保存在strClass中
+            string strClass = sb.ToString();
+
+            if (strName.IndexOf(txtAppTitle.Text) >= 0)
+            {
+                return false; //返回false中止EnumWindows遍历
+            }
+            else
+            {
+                return true; //返回true继续EnumWindows遍历
+            }
+        }
+
+        private void PressKey(Keys key)
+        {
+            const int KEYEVENTF_EXTENDEDKEY = 0x1;
+            const int KEYEVENTF_KEYUP = 0x2;
+            // I had some Compile errors until I Casted the final 0 to UIntPtr like this...
+            keybd_event((byte)key, 0x45, KEYEVENTF_EXTENDEDKEY, 0);
+            keybd_event((byte)key, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+        }
+
+        private void PressKeyArray(Keys[] keys)
+        {
+            foreach (Keys key in keys)
+            {
+                PressKey(key);
+            }
+        }
+
+        private void btnSendMouseKey_Click(object sender, EventArgs e)
+        {
+            int mouseKeyDown = (int)(cmbMouseKey.SelectedIndex == 0 ? WM_LBUTTONDOWN : cmbMouseKey.SelectedIndex == 1 ? WM_MBUTTONDOWN : WM_RBUTTONDOWN);
+            int mouseKeyUp = (int)(cmbMouseKey.SelectedIndex == 0 ? WM_LBUTTONUP : cmbMouseKey.SelectedIndex == 1 ? WM_MBUTTONUP : WM_RBUTTONUP);
+            int x = Convert.ToInt32(txtMouseX.Text);
+            int y = Convert.ToInt32(txtMouseY.Text);
+            int lparam = x | (y << 16);
+            SendMessage(hwnd, mouseKeyDown, 0, lparam);
+            SendMessage(hwnd, mouseKeyUp, 0, lparam);
+        }
+
+        private void btnSendKey_Click(object sender, EventArgs e)
+        {
+            int key = Convert.ToInt32(txtKey.Text);
+            SendMessage(hwnd, WM_KEYDOWN, key, 0);
+            SendMessage(hwnd, WM_KEYUP, key, 0);
+        }
+
+        private void txtKey_KeyDown(object sender, KeyEventArgs e)
+        {
+            txtKey.Text = e.KeyValue.ToString();
+        }
+
+        private void btnGroupSend_Click(object sender, EventArgs e)
+        {
+            int count = Convert.ToInt32(txtRepeatTimes.Text);
+            for (int t = 0; t < count; t++)
+            {
+                //txtMouseX.Text = "800";
+                //txtMouseY.Text = "150";
+                txtKey.Text = "33";
+                btnSendMouseKey_Click(null, null);
+                for (int i = 0; i < 10; i++)
+                {
+                    btnSendKey_Click(null, null);
+                }
+
+                //txtMouseX.Text = "600";
+                //txtMouseY.Text = "80";
+                //btnSendMouseKey_Click(null, null);
+            }
+        }
+    }
+}
